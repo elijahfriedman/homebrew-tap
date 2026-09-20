b2de2cb3783e82eaeaf1b050cb189d06bcf1004f
Elijah Friedman <efriedman810@gmail.com>
2026-09-20 09:27:28 -0400

    Update keypause for macOS Big Sur

--- Formula/keypause.rb
+++ Formula/keypause.rb
@@ -2,10 +2,10 @@ class Keypause < Formula
   desc "Lock your keyboard and trackpad until you press an activator combo"
   homepage "https://github.com/elijahfriedman/keypause"
   url "https://github.com/elijahfriedman/keypause/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "4361bb1649827ff36d0453c4b1de00c0f36a508f"
+  sha256 "28fa8c2dc9ed05c01aa337737f4d6b5574c14d4d04aa7f88c236b439d22d3fd8"
   license "MIT"
 
-  depends_on :macos
+  depends_on macos: :big_sur
 
   def install
     system "xcodebuild",
